3f3fcc72deb1ebeee6da305d3996daabfc896697
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-04-11 23:21:18 +0800

    Small modification

--- labs/src/base/StringHelper.java
+++ labs/src/base/StringHelper.java
@@ -9,12 +9,11 @@ public class StringHelper {
             return true;
         }
 
-        final char FIRST_TARGET_CHAR_LOWER = Character.toLowerCase(target.charAt(0));
-        final char FIRST_TARGET_CHAR_UPPER = Character.toUpperCase(target.charAt(0));
-
         for (int i = SOURCE_LENGTH - TARGET_LENGTH; i >= 0; --i) {
             // Quick check before calling the more expensive regionMatches() method:
-            char ch = source.charAt(i);
+            final char FIRST_TARGET_CHAR_LOWER = Character.toLowerCase(target.charAt(0));
+            final char FIRST_TARGET_CHAR_UPPER = Character.toUpperCase(target.charAt(0));
+            final char ch = source.charAt(i);
             if (ch != FIRST_TARGET_CHAR_LOWER && ch != FIRST_TARGET_CHAR_UPPER) {
                 continue;
             }
