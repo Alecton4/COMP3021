b835579c1ed1e2446e7afc321a7e4b6d82099421
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-05-25 23:56:20 +0800

    Finish assignment4

--- assignment4/src/main/java/Bank.java
+++ assignment4/src/main/java/Bank.java
@@ -7,17 +7,46 @@ interface Miner {
 }
 
 public interface Bank {
+	// add a new account 'accountID' to the bank and set its balance to
+	// 'initBalance'.
+	// Return true if 'accountID' is successfully added.
+	// Return false if 'accountID' already exists in the bank (in this case, we do
+	// not modify the
+	// bank and directly return false).
 	public boolean addAccount(String accountID, Integer initBalance);
 
+	// If 'accountID' is not yet added in the bank, directly return false;
+	// Otherwise, increase the balance for 'accountID' by 'amount' (i.e., perform a
+	// deposit to 'accountID') and return true.
 	public boolean deposit(String accountID, Integer amount);
 
+	// If ('accountID' is not yet added in the bank) then { return false }
+	// Else if (the balance for 'accountID' is no less than 'amount') then { deduct
+	// the balance by 'amount' and return true }
+	// Else {
+	// the current thread should wait for at most 'timeoutMillis' milliseconds (in
+	// case some other transactions add money to 'accountID').
+	// After the current thread wakes (either by a notify from another thread or
+	// after timeout), it should check
+	// again whether the current balance for 'accountID' is no less than 'amount'.
+	// If 'accountID' has enough money after waiting, deduct the balance by 'amount'
+	// and return true.
+	// Otherwise, return false.
+	// }
 	public boolean withdraw(String accountID, Integer amount, long timeoutMillis);
 
+	// Transfer 'amount' of money from 'srcAccount' to 'dstAccount'.
+	// This involves a withdraw from 'srcAccount' (with timeout 'timeoutMillis') and
+	// a deposit to 'dstAccount'.
+	// Return true if the transfer succeeds. Return false if any of the involved
+	// steps in transfer fails.
 	public boolean transfer(String srcAccount,
 			String dstAccount,
 			Integer amount,
 			long timeoutMillis);
 
+	// Return the current balance for 'accountID'. If 'accountID' is not added to
+	// Bank, return 0.
 	public Integer getBalance(String accountID);
 
 	public void doLottery(ArrayList<String> accounts, Miner miner);
--- assignment4/src/main/java/ConcreteBank.java
+++ assignment4/src/main/java/ConcreteBank.java
@@ -1,43 +1,185 @@
 package main.java;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ConcreteBank implements Bank {
 
+    // public static Map<String, Integer> accountsMapByID;
+    // public static Map<String, ReadWriteLock> locksMapByID;
+
+    // static {
+    // accountsMapByID = new HashMap<>();
+    // locksMapByID = new HashMap<>();
+    // }
+
+    private Map<String, Integer> accountsMapByID = new HashMap<>();
+    // private Map<String, Integer> accountsMapByID = new ConcurrentHashMap<>();
+    private Map<String, Lock> locksMapByID = new HashMap<>();
+    // private Map<String, Lock> locksMapByID = new ConcurrentHashMap<>();
+    // private Map<String, ReadWriteLock> locksMapByID = new HashMap<>();
+
+    // REVIEW
+    class TaskForMiner implements Runnable {
+        private String accountID;
+        private Miner miner;
+
+        public TaskForMiner(String accountID, Miner miner) {
+            this.accountID = accountID;
+            this.miner = miner;
+        }
+
+        @Override
+        public void run() {
+            deposit(this.accountID, this.miner.mine(this.accountID));
+        }
+    }
+
     @Override
     public boolean addAccount(String accountID, Integer initBalance) {
-        // TODO Auto-generated method stub
-        return false;
+        // REVIEW
+        synchronized (this) {
+            if (accountsMapByID.containsKey(accountID)) {
+                return false;
+            }
+
+            accountsMapByID.put(accountID, initBalance);
+            locksMapByID.put(accountID, new ReentrantLock());
+            return true;
+        }
     }
 
     @Override
     public boolean deposit(String accountID, Integer amount) {
-        // TODO Auto-generated method stub
-        return false;
+        // REVIEW
+        if (!accountsMapByID.containsKey(accountID)) {
+            return false;
+        }
+
+        Lock wLock = locksMapByID.get(accountID);
+
+        synchronized (wLock) {
+            Integer newBalance = accountsMapByID.get(accountID) + amount;
+            accountsMapByID.replace(accountID, newBalance);
+            wLock.notifyAll();
+            return true;
+        }
     }
 
     @Override
     public boolean withdraw(String accountID, Integer amount, long timeoutMillis) {
-        // TODO Auto-generated method stub
-        return false;
+        // REVIEW
+        if (!accountsMapByID.containsKey(accountID)) {
+            return false;
+        }
+
+        Lock wLock = locksMapByID.get(accountID);
+
+        synchronized (wLock) {
+            Integer currBalance = accountsMapByID.get(accountID);
+            if (currBalance < amount) {
+                // wait for some time
+                try {
+                    wLock.wait(timeoutMillis);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // update currBalance
+                currBalance = accountsMapByID.get(accountID);
+            }
+
+            if (currBalance >= amount) {
+                Integer newBalance = currBalance - amount;
+                accountsMapByID.replace(accountID, newBalance);
+                wLock.notifyAll();
+                return true;
+
+            } else {
+                wLock.notifyAll();
+                return false;
+            }
+        }
     }
 
     @Override
     public boolean transfer(String srcAccount, String dstAccount, Integer amount, long timeoutMillis) {
-        // TODO Auto-generated method stub
-        return false;
+        // REVIEW
+        // first we withdraw from srcAcc
+        if (!withdraw(srcAccount, amount, timeoutMillis)) {
+            return false;
+        }
+
+        // then we deposit to dstAcc
+        if (!deposit(dstAccount, amount)) {
+            // try to put money back to srcAcc
+            deposit(srcAccount, amount);
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public Integer getBalance(String accountID) {
-        // TODO Auto-generated method stub
-        return null;
+        // REVIEW
+        Integer currBalance = 0;
+        if (!accountsMapByID.containsKey(accountID)) {
+            return currBalance;
+        }
+
+        Lock rLock = locksMapByID.get(accountID);
+
+        synchronized (rLock) {
+            currBalance = accountsMapByID.get(accountID);
+            rLock.notifyAll();
+        }
+
+        return currBalance;
     }
 
     @Override
     public void doLottery(ArrayList<String> accounts, Miner miner) {
-        // TODO Auto-generated method stub
+        // REVIEW
+        // int numAccounts = accounts.size();
+        // ExecutorService es = Executors.newFixedThreadPool(numAccounts);
 
+        // for (String accountID : accounts) {
+        //     es.submit(new Thread(() -> {
+        //         deposit(accountID, miner.mine(accountID));
+        //     }));
+        // }
+
+        // es.shutdown();
+
+        List<Thread> threads = new ArrayList<>();
+        for (String accountID : accounts) {
+            threads.add(new Thread(() -> {
+                deposit(accountID, miner.mine(accountID));
+            }));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
-    // TODO
 }
